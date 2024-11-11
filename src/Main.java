e055d3b8004304221c7c18f52263410e3268936b
Forest <fhaughton6@gmail.com>
2024-11-11 08:15:09 -0500

    123

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        int yes = InputHelper.getInt(scan, "Enter your favorite int");
+        double no = InputHelper.getDouble(scan, "Enter your favorite double");
+        System.out.println("Your favorite int and double respectively is: " + yes + " and " + no);
     }
 }
